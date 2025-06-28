9e89e89197077177809afc50ada4a4433baa701b
prvtaxl <axlendraharissanjaya@gmail.com>
2025-06-29 00:12:49 +0700

    update 29-06-2025 00:12

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-        // NAVBAR
+       // Animasi Navbar saat scroll
         $(window).scroll(function() {
             if ($(this).scrollTop() > 50) {
                 $('.navbar').addClass('scrolled');
@@ -6,3 +6,15 @@
                 $('.navbar').removeClass('scrolled');
             }
         });
+        
+        // Animasi saat search bar di focus
+        $('.form-control').focus(function() {
+            $(this).css('width', '250px');
+        }).blur(function() {
+            if ($(window).width() > 991) {
+                $(this).css('width', '200px');
+            }
+        });
+
+        // Set tahun saat ini untuk footer
+        document.getElementById('year').textContent = new Date().getFullYear();
